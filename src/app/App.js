9163c38d37963009af9be361a1b3963a6036184c
Victor <victomas@outlook.es>
2021-01-18 10:13:42 +0100

    build

--- src/app/App.js
+++ src/app/App.js
@@ -4,20 +4,37 @@ import {getContries} from "./services/countryService";
 
 const App = () => {
   const [search, setSearch] = useState('');
+  const [countries, setCountries] = useState([]);
 
   const handlerChangeSearch = (e) => {
       setSearch(e.target.value);
       if(e.target.value.replaceAll(' ', '')) {
           getContries(e.target.value)
               .then((countries) => {
+                setCountries(countries);
                 console.log(countries)
               });
       }
+      else {
+          setCountries([]);
+      }
   }
   return (
-    <div className={'gj'}>
-      <input value={search} onChange={handlerChangeSearch}/>
-    </div>
+      <>
+          <div className={'gj'}>
+              <input value={search} onChange={handlerChangeSearch}/>
+          </div>
+          { countries.length > 0 ?
+              countries.map((value, index) => (
+                  <>
+                      <p> {value.name + ' - ' + value.capital} </p>
+                      <img src={value.flag} alt={'flag'} width={'250px'} />
+                  </>
+              ))
+              :
+              <> No hay datos que mostrar</>
+          }
+      </>
   );
 }
 
